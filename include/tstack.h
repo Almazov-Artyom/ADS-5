17da246f1dda60bfb6aadb1bd5410744c056b6a7
Almazov-Artyom <125147917+Almazov-Artyom@users.noreply.github.com>
2023-03-26 18:21:57 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,33 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[size];
+    int top;
+public:
+    TStack(): top(-1){}
+    void Push(T& value) {
+        if (!isFull()) {
+            arr[++top] = value;
+        }
+    }
+    T get() const {
+        if (!isEmpty()) {
+            return arr[top];
+        }
+    }
+    T pop() {
+        if (!isEmpty()) {
+            return arr[top--];
+        }
+    }
+    int GetTop() const { return top; }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
